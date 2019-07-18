659f940eb89d6455b98bff3f24bde0598e581fb0
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-07-18 11:10:31 +0900

    Fix spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@
 
 require 'bundler/setup'
 require 'jipcode'
-require 'jipcode-address_locator'
+require 'jipcode/address_locator'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
